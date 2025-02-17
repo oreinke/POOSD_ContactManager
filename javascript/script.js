691bb0136c56da6913b6f0d0b26a20c2fb3c0aa6
JayR-360 <149827065+JayR-360@users.noreply.github.com>
2025-02-17 03:24:22 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,10 @@
-const urlBase = 'http://www.whenthe.cc/';
+const urlBase = 'https://www.whenthe.cc/API';
 const extension = 'php';
 
 let userId = 0;
-let firstName = "";
-let lastName = "";
+let first_name = "";
+let last_name = "";
+
 
 function toggleAuth() {
     const authTitle = document.getElementById('auth-title');
@@ -34,7 +35,7 @@ function toggleAuth() {
             <div class="auth__group">
                 <label class="auth__label" for="password">Password</label>
                 <input class="auth__input" type="password" id="password" required>
-            </div>
+		            </div>
             <button type="submit" class="auth__button">Login</button>
         `;
         toggleText.textContent = "Don't have an account? Register";
@@ -43,52 +44,94 @@ function toggleAuth() {
 
 function doLogin()
 {
-	userId = 0;
-	firstName = "";
-	lastName = "";
-	
-	let login = document.getElementById("loginName").value;
-	let password = document.getElementById("loginPassword").value;
-//	var hash = md5( password );
-	
-	document.getElementById("loginResult").innerHTML = "";
 
-	let tmp = {login:login,password:password};
-//	var tmp = {login:login,password:hash};
-	let jsonPayload = JSON.stringify( tmp );
-	
-	let url = "http://52.44.189.230/";
+        userId = 0;
+        first_name = "";
+        last_name = "";
 
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				let jsonObject = JSON.parse( xhr.responseText );
-				userId = jsonObject.id;
-		
-				if( userId < 1 )
-				{		
-					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-					return;
-				}
-		
-				firstName = jsonObject.firstName;
-				lastName = jsonObject.lastName;
+        let login = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+//      var hash = md5( password );
 
-				saveCookie();
-	
-				window.location.href = "contacts.html";
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-	}
+        document.getElementById("loginResult").innerHTML = "";
+
+        let tmp = {login:login,password:password};
+//      var tmp = {login:login,password:hash};
+        let jsonPayload = JSON.stringify( tmp );
+
+        let url = urlBase + '/Login.' + extension;
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+        try
+        {
+                xhr.onreadystatechange = function()
+                {
+			 if (this.readyState == 4 && this.status == 200)
+                        {
+                                let jsonObject = JSON.parse( xhr.responseText );
+                                userId = jsonObject.id;
+
+                                if( userId < 1 )
+                                {
+                                        document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+                                        return;
+                                }
+
+                                first_name = jsonObject.first_name;
+                                last_name = jsonObject.last_name;
+
+                                saveCookie();
+
+                                window.location.href = "contacts.html";
+                        }
+                };
+                xhr.send(jsonPayload);
+        }
+        catch(err)
+        {
+                document.getElementById("loginResult").innerHTML = err.message;
+        }
+}
+
+function saveCookie()
+{
+        let minutes = 20;
+        let date = new Date();
+        date.setTime(date.getTime()+(minutes*60*1000));
+        document.cookie = "first_name" + first_name + ",last_name="  + last_name + ",userId=" + userId + ";expires=" + date + date.toGMTString();
+}
+
+function readCookie()
+{
+	  userId = -1;
+        let data = document.cookie;
+        let splits = data.split(",");
+        for(var i = 0; i < splits.length; i++)
+        {
+                let thisOne = splits[i].trim();
+                let tokens = thisOne.split("=");
+                if( tokens[0] == "first_name" )
+                {
+                        first_name = tokens[1];
+                }
+                else if( tokens[0] == "last_name" )
+                {
+                        last_name = tokens[1];
+                }
+                else if( tokens[0] == "userId" )
+                {
+                        userId = parseInt( tokens[1].trim() );
+                }
+        }
+
+        if( userId < 0 )
+        {
+                window.location.href = "index.html";
+        }
+        else
+        {
+//              document.getElementById("userName").innerHTML = "Logged in >
+        }
 }
