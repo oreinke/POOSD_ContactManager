cad7ca0b90c2d877f12009ed2911ba154bdda96f
JayR-360 <149827065+JayR-360@users.noreply.github.com>
2025-02-16 23:21:11 -0500

    Update Login.php

--- API/Login.php
+++ API/Login.php
@@ -1,62 +1,71 @@
 <?php
 
-    $inData = getRequestInfo();
+$inData = getRequestInfo();
 
-    $id = 0;
-    $firstName = "";
-    $lastName = "";
+$conn = new mysqli("localhost", "dba", "dbapass", "contact_manager");
+if ($conn->connect_error) 
+{
+    returnWithError($conn->connect_error);
+} 
+else
+{
+    $searchTerm = "%" . $inData["search"] . "%"; // Add wildcard for LIKE operator
+    $userId = $inData["userid"];
 
-    $conn = new mysqli("localhost", "dba", "dbapass", "contact_manager");
-    if ($conn->connect_error) 
+    $stmt = $conn->prepare("SELECT id, first_name, last_name, email FROM contacts 
+                            WHERE (first_name LIKE ? OR last_name LIKE ? OR email LIKE ?) 
+                            AND userid = ?");
+    
+    $stmt->bind_param("sssi", $searchTerm, $searchTerm, $searchTerm, $userId);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    $searchResults = [];
+    
+    while ($row = $result->fetch_assoc()) 
     {
-        returnWithError($conn->connect_error);
+        $searchResults[] = [
+            "id" => $row["id"],
+            "firstName" => $row["first_name"],
+            "lastName" => $row["last_name"],
+            "email" => $row["email"]
+        ];
+    }
+    
+    if (count($searchResults) > 0) 
+    {
+        returnWithInfo($searchResults);
     } 
-    else
+    else 
     {
-        $stmt = $conn->prepare("SELECT id, first_name, last_name, email FROM contacts 
-                                WHERE (first_name LIKE ? OR last_name LIKE ? OR email LIKE ?) 
-                                AND userid = ?");
-        
-        $stmt->bind_param("ss", $inData["login"], $inData["password"]);
-		$stmt->execute();
-		$result = $stmt->get_result();
-                
-        
-        if( $row = $result->fetch_assoc()  )
-		{
-			returnWithInfo( $row['firstName'], $row['lastName'], $row['id'] );
-		}
-		else
-		{
-			returnWithError("No Records Found");
-		}
-
-		$stmt->close();
-		$conn->close();
+        returnWithError("No Records Found");
     }
 
-    function getRequestInfo()
-    {
-        return json_decode(file_get_contents('php://input'), true);
-    }
+    $stmt->close();
+    $conn->close();
+}
 
-    function sendResultInfoAsJson($obj)
-    {
-        header('Content-type: application/json');
-        echo $obj;
-    }
-    
-    function returnWithError($err)
-    {
-        $retValue = '{"results":[],"error":"' . $err . '"}';
-        sendResultInfoAsJson($retValue);
-    }
-    
-    function returnWithInfo($searchResults)
-    {
-        $retValue = '{"results":[' . $searchResults . '],"error":""}';
-        sendResultInfoAsJson($retValue);
-    }
-    
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo json_encode($obj);
+}
+
+function returnWithError($err)
+{
+    $retValue = ["results" => [], "error" => $err];
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($searchResults)
+{
+    $retValue = ["results" => $searchResults, "error" => ""];
+    sendResultInfoAsJson($retValue);
+}
+
 ?>
-
