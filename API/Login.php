a4553d0c02f9fd90b98eaf565fd181ef268a1576
scinilol <diego2249a@gmail.com>
2025-02-25 00:30:02 -0500

    Update Login.php

--- API/Login.php
+++ API/Login.php
@@ -1,5 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json; charset=UTF-8");
+//header('Access-Control-Allow-Origin: *');
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
