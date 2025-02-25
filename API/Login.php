62b5b57347ac10a155d0400dae36280a1346ac9b
scinilol <diego2249a@gmail.com>
2025-02-25 00:48:56 -0500

    Update Login.php

--- API/Login.php
+++ API/Login.php
@@ -1,20 +1,21 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json; charset=UTF-8");
-//header('Access-Control-Allow-Origin: *');
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $inData = getRequestInfo();
 
-$id = 0;
-$first_name = "";
-$last_name = "";
+//$id = 0;
+//$first_name = "";
+//$last_name = "";
 
 $conn = new mysqli("localhost", "dba", "dbapass", "contact_manager");
 
 if ($conn->connect_error) {
     returnWithError($conn->connect_error);
-} else {
+    exit();
+}
     // Fix SQL query for user authentication
     $stmt = $conn->prepare("SELECT id, name  FROM users WHERE username=? AND password=?");
     $stmt->bind_param("ss", $inData["username"], $inData["password"]);
@@ -33,7 +34,6 @@ if ($conn->connect_error) {
 
     $stmt->close();
     $conn->close();
-}
 
 function getRequestInfo() {
     return json_decode(file_get_contents('php://input'), true);
@@ -50,10 +50,10 @@ function returnWithError($err) {
     	sendResultInfoAsJson($retValue);
 }
 
-function returnWithInfo($row['id'], $row['name'], "")) {
+function returnWithInfo($id, $name, $extraParam) {
 	//$retValue = '{"id":' . $id . ',"first_name":"' . $first_name . '","last_name":"' . $last_name . '","error":""}';
 	$retValue = '{"id":' . $id . ',"name":"' . $name . '","error":""}';
-    sendResultInfoAsJson($retValue);
+    	sendResultInfoAsJson($retValue);
 }
 
 
